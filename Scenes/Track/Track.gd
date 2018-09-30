84f8e903324d1f20e420b18f4a4e45982cee2dde
thefatsheep <danieldylanfer@bellsouth.net>
2018-09-30 01:02:12 -0400

    Added track scene

---
+++ Scenes/Track/Track.gd
@@ -0,0 +1,15 @@
+extends Sprite
+
+
+func _ready():
+#	movement()
+	pass
+
+#func _process(delta):
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+#	pass
+
+func movement(delta):
+	
+	pass
