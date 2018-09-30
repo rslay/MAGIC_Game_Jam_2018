de2cb8f6ea2d3681cecee9a2ddd38d891d57ccef
rslay <rhern283@fiu.edu>
2018-09-29 22:41:05 -0400

    Added player movement mechanics

--- Scenes/Characters/Character.gd
+++ Scenes/Characters/Character.gd
@@ -1,7 +1,6 @@
 extends KinematicBody2D
 
 
-const MAX_SPEED = 300
 const MAX_X_VELOCITY = 20
 
 
@@ -12,13 +11,13 @@ enum RAILPOS { TOP, MIDDLE, BOTTOM }
 # Character animations (switch means switching rails)
 enum ANIM { IDLE, ATTACK, SWITCH }
 
+const UP = Vector2(0, -1)
 
+# Acceleration speed
+var accel = 200
 
 # The horizontal position the player is at in the track
 var xpos = 0.5
-
-# Current player mph, their "health"
-var speed = 50
 
 # Current player horizontal movement speed within range of scene
 var velocity = 0
@@ -29,8 +28,11 @@ var distance = 0
 # Player's current state, aka animations
 var state = null
 
+# Player's current rail (1=top, 2=mid, 3=bot)
+var rail = null
 
 
+# Changes the character state animation
 func changeState(newState):
 	
 	match newState:
@@ -42,5 +44,29 @@ func changeState(newState):
 			$AnimationPlayer.play('switch')
 	
 	state = newState
+	pass
+
+
+# Switch to a specific rail or move to a rail relative to the character
+func changeRail(direction):
+	
+	changeState(SWITCH)
+	
+	# If the character is moving to a specific rail
+	if direction == RAILPOS.TOP:
+		rail = 1
+	elif direction == RAILPOS.MIDDLE:
+		rail = 2
+	elif direction == RAILPOS.BOTTOM:
+		rail = 3
+		
+	# If the character is moving up or down
+	elif direction == "UP":
+		rail = min(rail - 1, 1)
+	elif direction == "DOWN":
+		rail = max(rail + 1, 3)
+	else:
+		ERR_PARAMETER_RANGE_ERROR('The character can only move up, down, or to a specific rail (RAILPOS.TOP, etc)')
+	
 	pass
 
--- Scenes/Characters/Player.gd
+++ Scenes/Characters/Player.gd
@@ -1,10 +1,24 @@
-extends "res://Scenes/Characters/Character.tscn"
+extends "res://Scenes/Characters/Character.gd"
 
-
+func movePlayer():
+	if Input.is_action_just_pressed('up'):
+		changeRail("UP")
+	elif Input.is_action_just_pressed('down'):
+		changeRail("DOWN")
+	
+	if Input.is_action_pressed('left'):
+		self.move_and_slide(Vector2(-accel, 0), UP)
+	elif Input.is_action_pressed('right'):
+		self.move_and_slide(Vector2(accel, 0), UP)
 
 func _ready():
-	# Called when the node is added to the scene for the first time.
-	# Initialization here
+	changeState(ANIM.IDLE)
+	rail = 2
+	pass
+
+func _physics_process(delta):
+	movePlayer()
+	
 	pass
 
 #func _process(delta):
